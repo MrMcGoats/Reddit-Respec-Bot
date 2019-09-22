d6aa8242e4ac52970b7be691378b8bfe65a112fd
Jacque McGoats <31115470+MrMcGoats@users.noreply.github.com>
2019-09-21 19:24:03 -0700

    Updated readability

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import praw
 from os import environ
 from sys import stderr
 
-def main():
+def respec(subreddit_name=environ['SUBREDDIT'],keyword=environ['FILTER']):
     
     try:
         bots=numpy.load("bots.npy",allow_pickle=True)
@@ -12,23 +12,36 @@ def main():
         bots=[{'id':'bot1','banned':False}]
 
     reddit=None
-    iterator=0
+    subreddit=None
+    new_bots=[]
     for i in bots:
         if not i['banned']:
             reddit=praw.Reddit(i['id'])
-            if reddit.subreddit(environ['SUBREDDIT']).banned(redditor=reddit.user.me()):
-                bots[iterator]['banned']==True
+            subreddit=reddit.subreddit(subreddit)
+            if subreddit.banned(redditor=reddit.user.me()):
+                new_bots.append({'id':i['id'],'banned':True})
             else:
+                new_bots.append({'id':i['id'],'banned':False})
                 break
-        iterator+=1
-        
-    if reddit.subreddit(environ['SUBREDDIT']).banned(redditor=reddit.user.me()):
-        print("Cannot continue; all acounts have been banned", file=stderr)
-        exit(1)
+    bots=new_bots
+    
+    try:
+        if subreddit.banned(redditor=reddit.user.me()):
+            print("Cannot continue; all accounts have been banned", file=stderr)
+            numpy.save("bots.npy",bots,allow_pickle=True)
+            exit(1)
+    except AttributeError as e:
+        if str(e)=="'NoneType' object has no attribute 'banned'":
+            print("Cannot continue; all accounts have been banned", file=stderr)
+            numpy.save("bots.npy",bots,allow_pickle=True)
+            exit(1)
+        print("Unknown error. RIP.",file=stderr)
+        exit(2)
 
+
+    
     numpy.save("bots.npy",bots,allow_pickle=True)
     
-    subreddit=reddit.subreddit(environ['SUBREDDIT'])
     
     posts_replied=None
     try:
@@ -37,7 +50,7 @@ def main():
         posts_replied=[]
     
     for i in subreddit.new(limit=5):
-        if environ["FILTER"].upper() in i.title.upper():
+        if keyword.upper() in i.title.upper():
             if i.id not in posts_replied:
                 done=False
                 while not done: #This waits for the next allowed request. Reddit ratelimits
@@ -52,4 +65,4 @@ def main():
     numpy.save("posts_replied.npy",posts_replied,allow_pickle=False)
     
 if __name__=="__main__":
-    main()
+    while True:respec()
